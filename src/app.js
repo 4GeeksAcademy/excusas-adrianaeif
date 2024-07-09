1284e4988cf15d7c3728195f7a87f9f1d3641f3c
adrianaeif <adrianaisea02@gmail.com>
2024-07-09 02:55:20 +0000

    Generador-Excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function aleatoriolista(lista) {
+    let numeroaleatorio = Math.floor(Math.random() * lista.length);
+    return lista[numeroaleatorio] + " ";
+  }
+
+  let who = ["Mi gato Sergio", "Patricia", "Mi papa", "Mi computador"];
+  let action = ["comió", "revisó", "rompió"];
+  let what = ["mi tarea", "mi casa", "mi té"];
+  let when = ["antes de clase", "ayer", "esta semana"];
+
+  console.log(
+    aleatoriolista(who) +
+      aleatoriolista(action) +
+      aleatoriolista(what) +
+      aleatoriolista(when)
+  );
 };
